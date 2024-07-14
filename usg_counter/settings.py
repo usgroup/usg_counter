222d676296536d14cd75593204247c9a4a27a980
Abdulhamid51 <73017095+Abdulhamid51@users.noreply.github.com>
2024-07-14 14:30:32 +0500

    some

--- usg_counter/settings.py
+++ usg_counter/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
